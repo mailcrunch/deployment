d10b478b4391aa95be2330ddce945ba677782961
hackystack <dorpalenbarry@gmail.com>
2014-07-14 19:23:08 -0700

    Working on auth, added passport google stategy logic into config.js server file

--- server/main/config.js
+++ server/main/config.js
@@ -4,13 +4,15 @@ var mongoose    = require('mongoose'),
     morgan      = require('morgan'),
     bodyParser  = require('body-parser'),
     middle      = require('./middleware'),
-    passport    = require('passport');
+    passport    = require('passport'),
+    passport = require('passport'),
+    GoogleStrategy = require('passport-google').Strategy;
 
 mongoose.connect(process.env.DB_URL || 'mongodb://localhost/myApp');
 /*
  * Include all your global env variables here.
 */
-module.exports = exports = function (app, express, routers) {
+module.exports = exports = function (app, express, routers, passport) {
   app.set('port', process.env.PORT || 3000);
   app.set('base url', process.env.URL || 'http://localhost');
   app.use(morgan('dev'));
@@ -23,4 +25,16 @@ module.exports = exports = function (app, express, routers) {
   app.use(middle.logError);
   app.use(middle.handleError);
 };
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost/auth/google/return',
+    realm: 'http://localhost'
+  },
+  function(identifier, profile, done) {
+    User.findOrCreate({ openId: identifier }, function(err, user) {
+      done(err, user);
+    });
+  }
+));
+
 

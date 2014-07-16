f46470232a84dc6281199f6e200ef36f89844df4
forrestbthomas <forrestbthomas@gmail.com>
2014-07-16 15:31:03 -0700

    Starting comments

--- client/common/factories.js
+++ client/common/factories.js
@@ -2,11 +2,15 @@
   "use strict";
   angular.module('myApp')
 
+  // This factory keeps track of the points a user has
   .factory('PointFactory', function(){
     var pointTotal = 0;
+    // This function is called any time a user sorts or crunches an email
+    // in the allotted time
     var incrementPoints = function(num){
       pointTotal += num;
-    }
+    };
+    // This function is called in client/main/main.js
     var getPoints = function(){
       return pointTotal;
     };
@@ -16,6 +20,8 @@
     }
   })
 
+  // This factory is used to ...
+  // TODO: Liev will comment this out
   .factory('Inbox', function($http){
     var sortedInbox = [];
     var getSortedInbox = function(){
@@ -28,20 +34,16 @@
         return response;
       });      
     }
-    var shiftQ = function(){
-      sortedInbox.shift();
-    };
 
     return {
-      getSortedInbox: getSortedInbox,
-      sortedInbox: sortedInbox,
-      shiftQ: shiftQ
+      getSortedInbox: getSortedInbox
     }
   })
 
+  // This factory sends a GET request to the server to retrieve emails via IMAP
+  // See server/note/note_routes.js then server/note/note_controllers.js
   .factory('InboxFactory', function($http){
       var getEm = function(){
-        console.log('got to the factory')
         return $http({
           method: 'GET',
           url: '/main/sort'

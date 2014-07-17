97ed9fedbc13767efd7bbb35a79c280ca2069c6b
liev nano <lievnano@gmail.com>
2014-07-16 19:04:05 -0700

    worked on converting to oauth2 from passport-google redirect not working

--- server/main/app.js
+++ server/main/app.js
@@ -3,8 +3,9 @@
 var express = require('express');
 var app = express(),
     passport = require('passport'),
-    GoogleStrategy = require('passport-google-oauth').Strategy;
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 require('./config.js')(app, express, passport, GoogleStrategy);
+passport.authenticate('google', {scope: 'https://www.googleapis.com/auth/plus.login'});
 
 module.exports = exports = app;

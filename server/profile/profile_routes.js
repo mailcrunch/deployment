79f86b20c8231cd0296a10a4826b1bc66c7773ad
hackystack <dorpalenbarry@gmail.com>
2014-07-20 15:54:31 -0700

    Response is successfully coming through

--- server/profile/profile_routes.js
+++ server/profile/profile_routes.js
@@ -5,5 +5,4 @@ var controller = require('./profile_controllers.js');
 
 module.exports = exports = function (app) {
   app.post('/data', controller.getProfile);
-  app.post('/main/profile/data', controller.getProfile);
 };

bc04ff7b20940386c5a0a678d979e122c8276a1a
hackystack <dorpalenbarry@gmail.com>
2014-07-20 15:48:46 -0700

    Updated routes in profile server documents

--- server/profile/profile_controllers.js
+++ server/profile/profile_controllers.js
@@ -5,27 +5,6 @@ var mongoClient = require('mongodb').MongoClient,
     auth        = require('../main/auth.js'); // For importing the clientId and secret for getting emails
 
 module.exports = exports = {
-  post: function(req,res,next){
-    if (!req.session.user){
-      res.redirect('/#/login');
-    }
-    var username = req.session.user;
-    try {
-      mongoClient.connect(auth.dbAuth.dbUri, function(err, db) {
-        if (err) throw err;
-        var collection = db.collection('users');
-        collection.find({username:username}).toArray(function(err,profileData){
-          if (err) throw err;
-          res.end(JSON.stringify("success!"));
-        });
-      });
-    }
-      catch (e){
-        console.log(e);
-      }
-    }
-  },
-
   getProfile: function(req,res,next){
     if (!req.session.user){
       res.redirect('/#/login');

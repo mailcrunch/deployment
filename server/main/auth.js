77a7298fda2cf6ae41b0e4a9000186f31bfea10c
liev nano <lievnano@gmail.com>
2014-07-21 11:20:17 -0700

    changed auth file for github

--- server/main/auth.js
+++ server/main/auth.js
@@ -1,3 +1,5 @@
+///DO NOT COMMIT SECURE DATA HERE!
+
 module.exports = {
   'googleAuth' : {
     'clientID'    : 'googleClientAPI key goes here',
@@ -6,6 +8,7 @@ module.exports = {
   },
   'dbAuth' : {
     'dbUri' : 'mongodb://localhost:27017/mailcrunch2' //this needs to change to mongolab db for deployment
-  }
+  },
+  'sessionSecret':'Put-Session-Secret-Here' //changeme when deploying live
 };
 

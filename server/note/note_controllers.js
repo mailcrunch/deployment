5e627a1f4631f6e500d8e287f515e2b7a0b43c1a
C. Moss <caly.moss@gmail.com>
2014-07-24 12:03:19 -0700

    fix mailparser and ngsanitize to display emails properly

--- server/note/note_controllers.js
+++ server/note/note_controllers.js
@@ -238,7 +238,10 @@ module.exports = exports = {
                       console.log(currentParsedEmail);
 
                       var message = {body: currentParsedEmail.html, headers: currentParsedEmail.headers, uid: UID};
+<<<<<<< HEAD
 
+=======
+>>>>>>> fix mailparser and ngsanitize to display emails properly
                       //add individual email to database with appropriate tags if it is not currently in db
 
                       var collection = db.collection('emails');

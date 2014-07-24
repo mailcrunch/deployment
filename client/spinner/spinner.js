18966708ecbb3956d1709b1e1b3d84bfe61e58dc
Justin Cheung <jygsaw@gmail.com>
2014-07-23 23:10:11 -0700

    (fix) App crashes when no unseen messages retrieved

--- client/spinner/spinner.js
+++ client/spinner/spinner.js
@@ -16,7 +16,7 @@
     .then(function(response){
       if (response === 'false'){
         $state.transitionTo('myApp.public.login')
-      } else {   
+      } else {
       InboxFactory.getEm()
         .then(function(response){
       console.log('got here')
@@ -31,40 +31,44 @@
           // clear inbox before populating with messages
           Inbox.clear();
 
-          for (var i = 0; i < response.data.length; i++){
-            if (response.data[i].headers.from !== undefined){
-              $scope.email = {
-                status: 'pending',
-                bucket: null
-              };
-            if (response.data[i].headers.subject === undefined){
-              $scope.email.subject = 'no subject';
-            } else {
-              $scope.email.subject = response.data[i].headers.subject.toString();
-            }
-            if (response.data[i].body === undefined){
-              $scope.email.body = 'no message contents';
-            } else {
-              if (response.data[i].body.indexOf('<b>') > -1){
-                response.data[i].body = response.data[i].body.slice(response.data[i].body.indexOf('<b>') + 3, response.data[i].body.lastIndexOf('</b>'));
+          // process new messages if found
+          // MailCrunch server responds with "no messages today" when no unseen messages found
+          if (response.data !== 'no messages today') {
+            for (var i = 0; i < response.data.length; i++){
+              if (response.data[i].headers.from !== undefined){
+                $scope.email = {
+                  status: 'pending',
+                  bucket: null
+                };
+              if (response.data[i].headers.subject === undefined){
+                $scope.email.subject = 'no subject';
+              } else {
+                $scope.email.subject = response.data[i].headers.subject.toString();
               }
-              $scope.email.body = response.data[i].body;
+              if (response.data[i].body === undefined){
+                $scope.email.body = 'no message contents';
+              } else {
+                if (response.data[i].body.indexOf('<b>') > -1){
+                  response.data[i].body = response.data[i].body.slice(response.data[i].body.indexOf('<b>') + 3, response.data[i].body.lastIndexOf('</b>'));
+                }
+                $scope.email.body = response.data[i].body;
+              }
+                $scope.email.to = response.data[i].headers.to.toString();
+                $scope.email.from = response.data[i].headers.from.toString();
+                $scope.email.time = response.data[i].headers.date.toString();
+                $scope.email.id = response.data[i].uid.toString();
+                $scope.email._id = response.data[i]._id;
+                Inbox.inbox.push($scope.email); 
+              }
             }
-              $scope.email.to = response.data[i].headers.to.toString();
-              $scope.email.from = response.data[i].headers.from.toString();
-              $scope.email.time = response.data[i].headers.date.toString();
-              $scope.email.id = response.data[i].uid.toString();
-              $scope.email._id = response.data[i]._id;
-              Inbox.inbox.push($scope.email); 
-            }
+            //temporary bug fix for double emails?
+            // if (Inbox.inbox.length > response.data.length){
+            //   Inbox.inbox.length = response.data.length;
+            // }
+            $timeout(function(){
+              $state.transitionTo('myApp.main.note')
+            }, 4000);
           }
-          //temporary bug fix for double emails?
-          // if (Inbox.inbox.length > response.data.length){
-          //   Inbox.inbox.length = response.data.length;
-          // }
-          $timeout(function(){
-            $state.transitionTo('myApp.main.note')
-          }, 4000)
         })
       }
     })

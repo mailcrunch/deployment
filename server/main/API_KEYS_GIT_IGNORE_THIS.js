55ae1ce98ad273ef8feb44e2307d09dd91a23db9
Forrest Thomas <forrestbthomas@gmail.com>
2014-08-11 14:39:58 -0700

    update

--- server/main/API_KEYS_GIT_IGNORE_THIS.js
+++ server/main/API_KEYS_GIT_IGNORE_THIS.js
@@ -1,10 +1,1 @@
-module.exports = {
-  'googleAuth' : {
-  'clientID' : '897990613553-diq6nbojlmjv36eqv5u3tq3qplf4c70q.apps.googleusercontent.com',
-  'clientSecret' : 'pq2tWxsCgmVKWZsVEDJ94qH6',
-  'callbackURL' : 'http://localhost:3000/auth/google/callback',
-  },
-  'dbAuth' : {
-    'dbUri' : 'mongodb://localhost:27017/mailcrunch2'
-  }
-};
+
